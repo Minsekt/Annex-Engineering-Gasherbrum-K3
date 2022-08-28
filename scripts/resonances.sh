de612d1532fbf1ebc07c9cb494b1ac1706ba8bbb
Minsekt <info@grigo.email>
2022-08-28 18:11:50 +0200

    Autocommit from 2022-08-28 18:11:50
    Klipper on commit: f7e29b2
    
    Moonraker on commit: e3cafb5
    
    Fluidd version: v1.19.1

---
+++ scripts/resonances.sh
@@ -0,0 +1,2 @@
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_x.png
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_y.png
